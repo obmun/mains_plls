23aa9cb7bb10aae1378f11ba51dbf7fae8a02f11
Jacobo Cabaleiro <obmun.h@gmail.com>
2007-07-20 09:32:06 +0200

    Correction in kcm. Xilinx and ModelSim were giving warnings

--- kcm.vhd
+++ kcm.vhd
@@ -12,10 +12,12 @@
 --
 -- Dependencies:
 -- 
--- Revision:
--- | Revision 0.02 - Modified constant generic to be able to compile in ModelSim
--- | Revision 0.01 - File Created
--- Additional Comments:
+-- *** Changelog ***
+-- Revision 0.03 - Corrected stupid problem with multiplication assignment
+-- Revision 0.02 - Modified constant generic to be able to compile in ModelSim
+-- Revision 0.01 - File Created
+--
+-- *** TODO ***
 -- | > TODO: see if this is optimized using precomputed sub products (using small ROMs as LUTs; o sea, usando LUTs :)). Comprobado: aparentemente NO => Tengo que implementarlo YO de forma manual.
 -- | > TODO: por qué está infiriendo un sumador?? Revisar el código <- DONE, a 
 -- | > TODO: test bench this design
@@ -30,22 +32,23 @@ entity kcm is
 	generic (
 		width : natural := PIPELINE_WIDTH;
 		prec : natural := PIPELINE_PREC;
-		k : pipeline_integer := -23410 -- Cte de ejemplo. Una síntesis de esta cte infiere un multiplicador
+		k : pipeline_integer := EXAMPLE_VAL_FX_316 -- Cte de ejemplo. Una síntesis de esta cte infiere un multiplicador
 	);
 	port (
 		i : in std_logic_vector(width - 1 downto 0);
-		o : out std_logic_vector(width - 1 downto 0)
-	);
+		o : out std_logic_vector(width - 1 downto 0));
 end kcm;
 
 architecture alg of kcm is
 begin
 	process(i)
 		constant k_signed : signed(width - 1 downto 0) := to_signed(k, width);
-		variable res : signed(2 * width - 2 downto 0);
+		variable res : signed(2 * width - 1 downto 0);
 		variable j_out : natural;
 	begin
-		res := signed(i) * k_signed; -- XST is STUPID. numeric_std signed * signed RETURNS (in own standard words) a'lenth * b'length - 1 (double sign bit) result. But at compile time ... it's giving a warning!!!
+		res := signed(i) * k_signed; -- Mmm... numeric_std signed * signed RETURNS (in own standard words) a'length * b'length - 1 (double sign bit!) result.
+                                             -- I'm really wasting a bit. Why?
+                                             -- Its just stupid ...
 		-- Manual shift for correct truncation
 		j_out := 0;
 		for i in prec to prec + width - 2 loop
