050663cc24405299d59a2dab1e5f0907d865f4eb
Jacobo Cabaleiro <obmun.h@gmail.com>
2007-07-23 12:53:22 +0200

    Modified sequential interface for freq2phase entity. Untested (TB not updated yet)

--- freq2phase.vhd
+++ freq2phase.vhd
@@ -10,6 +10,7 @@
 -- Tool versions:  
 --
 -- *** BRIEF DESCRIPTION ***
+-- Class: sequential.
 -- Entity that transforms freq + phase error info to instant phase for
 -- normalized signal generation. Return angle is in range - PI < angle <= PI
 -- Works on rising clock edge and has synchronous reset.
@@ -35,17 +36,14 @@
 --    2*pi*50 = 314.16
 --    Solution (lets supose Fs = 10 Khz): 2*pi*50 * SAMPLE_PERIOD = 0.031416!!!!
 -- 
--- ** SPEED IMPLEMENTATION **
--- Problem with speed: in route fa -> pi_kp -> freq2phase there is a BIG delay.
--- To solve it a new register was added in this element so speed was increased.
--- Measurement (synthgesis clock report) showed that inserting it AFTER:
--- -> in_adder produced a phase_loop max freq of 47.5 MHz
--- -> in_kcm produced a phase_loop max freq of 55.608 MHz
--- -> in produced a phase_loop max freq of 55.608 MHz (explained as in kcm is set to X"0001"
+-- ** SPEED **
+-- There is a big combinational depth in the path fa -> pi_kp -> freq2phase.
+-- Must be corrected outside this element.
 --
 -- Dependencies:
 -- 
 -- *** Revision ***
+-- Revision 0.05 - Added new seq. block control iface. Removed the speed up register.
 -- Revision 0.04 - Added run / done style ports
 -- Revision 0.03 - Integrated extra register to achieve better peformance (this had a hot path with
 --                 to much logic levels).
@@ -53,11 +51,7 @@
 -- Revision 0.01 - First version
 --
 -- *** TODO ***
--- * Speed up register SHOULD NOT BE INSIDE THIS FUCKING ELEMENT!!! Should be
--- outside, in the pipe. WTF was I'm thinking when I implemented this!
 --
--- Additional Comments:
--- 
 --------------------------------------------------------------------------------
 library IEEE;
 use WORK.COMMON.all;
@@ -67,28 +61,28 @@ use IEEE.NUMERIC_STD.all;
 entity freq2phase is
         -- rev 0.04
 	generic (
-		width : natural := PIPELINE_WIDTH);
+		width : natural := PIPELINE_WIDTH;
+                -- Seq. block iface
+                delayer_width : natural := 1);
+        
 	port (
-		clk, en, rst : in std_logic;
+		clk, rst : in std_logic;
 		f : in std_logic_vector(width - 1 downto 0);
 		p : out std_logic_vector(width - 1 downto 0);
-                run : in std_logic;
-                done : out std_logic);
+                run_en : in std_logic;
+                run_passthru : out std_logic;
+                delayer_in : in std_logic_vector(delayer_width - 1 downto 0);
+                delayer_out : out std_logic_vector(delayer_width - 1 downto 0));
 end freq2phase;
 
-architecture alg of freq2phase is
-        type state_t is (
-                ST_LOAD, ST_STORE);
-
-        signal st : state_t;
-    
+architecture beh of freq2phase is       -- behavioural
         -- Component declarations
         component kcm is
                 generic (
                         width : natural := PIPELINE_WIDTH;
                         prec : natural := PIPELINE_PREC;
-                        k : pipeline_integer := -23410 -- Cte de ejemplo. Una síntesis de esta cte infiere un multiplicador
-                        );
+                        -- Cte de ejemplo. Una síntesis de esta cte infiere un multiplicador
+                        k : pipeline_integer := EXAMPLE_VAL_FX316);
                 port (
                         i : in std_logic_vector(width - 1 downto 0);
                         o : out std_logic_vector(width - 1 downto 0));
@@ -133,7 +127,7 @@ architecture alg of freq2phase is
         -- Signals
         signal gt_pi : std_logic;
         signal reg_out, speed_reg_out, kcm_out, fb_adder_out, in_adder_out, pi_limit_out : std_logic_vector(width - 1 downto 0);
-        signal speed_reg_we_s, reg_we_s : std_logic;
+        signal delayer_in_s, delayer_out_s : std_logic_vector(delayer_width - 1 downto 0);
 
 begin
 	k_gt_comp_i : k_gt_comp
@@ -145,25 +139,18 @@ begin
 			a_gt_k => gt_pi
 		);
 
-	speed_reg_i : reg
-		port map (
-			clk => clk, we => speed_reg_we_s, rst => rst,
-			i => f,
-			o => speed_reg_out
-		);
-
 	kcm_i : kcm
 		generic map (
 			k => SAMPLE_PERIOD_FX316
 		)
 		port map (
-			i => speed_reg_out,
+			i => f,
 			o => kcm_out
 		);
 
 	in_adder : adder
 		port map (
-			a => speed_reg_out,
+			a => f,
 			b => std_logic_vector(AC_FREQ_SAMPLE_SCALED_FX316_S),
 			o => in_adder_out
 		);
@@ -177,7 +164,7 @@ begin
 
 	reg_i : reg
 		port map (
-			clk => clk, we => reg_we_s, rst => rst,
+			clk => clk, we => run_en, rst => rst,
 			i => pi_limit_out, o => reg_out
 		);
 
@@ -188,44 +175,27 @@ begin
 			o => pi_limit_out
 		);
 
+        delayer : reg
+                generic map (
+                        width => delayer_width)
+		port map (
+			clk => clk, we => '1', rst => rst,
+			i => delayer_in_s,
+			o => delayer_out_s);
+
+        single_delayer_gen: if (delayer_width = 1) generate
+                delayer_in_s(0) <= run_en;
+                run_passthru <= std_logic(delayer_out_s(0));
+                delayer_out(0) <= delayer_out_s(0);
+        end generate single_delayer_gen;
+
+        broad_delayer_gen: if (delayer_width > 1) generate
+                delayer_in_s(0) <= run_en;
+                delayer_in_s(delayer_width - 1 downto 1) <= delayer_in(delayer_width - 1 downto 1);
+                run_passthru <= std_logic(delayer_out_s(0));
+                delayer_out <= delayer_out_s;
+        end generate broad_delayer_gen;
+                
 	p <= reg_out;
 
-        st_ctrl : process(clk)
-        begin
-                if (rising_edge(clk)) then
-                        if (rst = '1') then
-                                st <= ST_LOAD;
-                        else
-                                if (en = '1') then
-                                        case st is
-                                                when ST_LOAD =>
-                                                        if (run = '0') then
-                                                                st <= ST_LOAD;
-                                                        else
-                                                                st <= ST_STORE;
-                                                        end if;
-                                                when ST_STORE =>
-                                                        st <= ST_LOAD;
-                                                when others =>
-                                                        null;
-                                        end case;                     
-                                end if;
-                        end if;
-                end if;
-        end process st_ctrl;
-
-        signal_gen : process(st)
-        begin
-                case st is
-                        when ST_LOAD =>
-                                speed_reg_we_s <= en and run;
-                                reg_we_s <= '0';
-                                done <= '0';
-                        when ST_STORE =>
-                                speed_reg_we_s <= '0';
-                                reg_we_s <= en;
-                                done <= '1';
-                        when others => null;
-                end case;
-        end process signal_gen;
-end alg;
+end beh;
