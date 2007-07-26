70cf4915afadfb918c6c356ecff201e49f27c4ff
Jacobo Cabaleiro <obmun.h@gmail.com>
2007-07-26 14:35:03 +0200

    More general routines in common for conversion of reals to integers and vectors. More constants transformed (some of them need to be recalculated)

--- common.vhd
+++ common.vhd
@@ -1,27 +1,37 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
-use IEEE.MATH_REAL.all;                 -- round function, and others?
+use IEEE.MATH_REAL.all;                 -- round function, ** (power), and ...
+                                        -- others?
 
 package common is
-	-- *
-	-- * My types
-	-- *
-	subtype pipeline_integer is integer range -32768 to 32767;
+
 	type shift_dir_t is (SD_LEFT, SD_RIGHT);
 
 	-- *
-	-- * Constants
+	-- * Very first constants
 	-- *
-	-- Non math constants
 	constant PIPELINE_WIDTH : natural := 16;
 	constant EXT_PIPELINE_WIDTH : natural := 18;
 	constant PIPELINE_PREC : natural := 12;
         constant PIPELINE_PREC_WEIGHT : natural := 4096;
 	constant EXT_PIPELINE_PREC : natural := PIPELINE_PREC;
+        constant EXT_PIPELINE_PREC_WEIGHT : natural := PIPELINE_PREC_WEIGHT;
 	constant PIPELINE_MAGN : natural := PIPELINE_WIDTH - PIPELINE_PREC;
 	constant EXT_PIPELINE_MAGN : natural := EXT_PIPELINE_WIDTH - EXT_PIPELINE_PREC;
 	constant PIPELINE_WIDTH_DIR_BITS : natural := 4; -- natural(ceil(log2(real(PIPELINE_WIDTH))));
+
+        -- *
+	-- * My pipeline types
+	-- *
+	subtype pipeline_integer is integer range -32768 to 32767;
+        subtype ext_pipeline_integer is integer range -131073 to 131072;
+        subtype pipeline_vector is std_logic_vector(PIPELINE_WIDTH - 1 downto 0);
+        subtype ext_pipeline_vector is std_logic_vector(EXT_PIPELINE_WIDTH - 1 downto 0);
+
+        --
+        -- MORE constants
+        --
 	-- CURRENT CONFIG:
 	-- >> SAMPLE RATE: 10 KHz
 	constant SAMPLE_PERIOD_FX316 : pipeline_integer := 1; -- NOT ENOUGH PRECISION: 0.8192!! BE CAREFULL!!
@@ -36,58 +46,69 @@ package common is
         constant EXAMPLE_VAL_FX416 : pipeline_integer := 26312;
 	constant ZERO_FX316_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := (others => '0');
         constant ZERO_FX416_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := (others => '0');
-	constant CORDIC_GAIN : pipeline_integer := 13490;
-	constant CORDIC_GAIN_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"34B2";
-        -- constant CORDIC_GAIN : pipeline_integer := 13490;
-	-- constant CORDIC_GAIN_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"34B2";
-	constant INV_CORDIC_GAIN : pipeline_integer := 4975;
-	constant INV_CORDIC_GAIN_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"136F";
-	constant MINUS_INV_CORDIC_GAIN : pipeline_integer := -4975;
-	constant MINUS_INV_CORDIC_GAIN_V : std_logic_vector(PIPELINE_WIDTH -1 downto 0) := X"EC91";
-	constant HALF_PI_FX316 : pipeline_integer := 12868;
-	constant HALF_PI_FX316_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"3244";
-        constant HALF_PI_FX416 : pipeline_integer := 6434;
-	constant HALF_PI_FX416_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"1922";
-        constant HALF_PI_FX318_V : std_logic_vector(EXT_PIPELINE_WIDTH - 1 downto 0) := "000011001001000100";
-	constant MINUS_HALF_PI_FX316 : pipeline_integer := -12868;
-	constant MINUS_HALF_PI_FX316_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"CDBC";
-        constant MINUS_HALF_PI_FX416 : pipeline_integer := -6434;
-	constant MINUS_HALF_PI_FX416_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"E6DE";
-        constant MINUS_HALF_PI_FX318_V : std_logic_vector(EXT_PIPELINE_WIDTH - 1 downto 0) := "111100110110111100";
-	constant PI_FX316 : pipeline_integer := 25736;
-	constant PI_FX316_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"6488";
-        constant PI_FX416 : pipeline_integer := 12868;
-	constant PI_FX416_V : std_logic_vector(PIPELINE_WIDTH - 1 downto 0) := X"3244";
-        constant PI_FX318_V : std_logic_vector(EXT_PIPELINE_WIDTH - 1 downto 0) := "000110010010001000";
-	constant MINUS_PI_FX316 : pipeline_integer := -25736;
-        constant MINUS_PI_FX318_V : std_logic_vector(EXT_PIPELINE_WIDTH - 1 downto 0) := "111001101101111000";
-	constant MINUS_TWO_PI_FX417_V : std_logic_vector(PIPELINE_WIDTH downto 0) := B"1_001_10110_11110000";
+	constant CORDIC_GAIN : real := 1.64672851562500;  -- RECALCULATE ME
+	constant INV_CORDIC_GAIN : real := 0.60729980468750;  -- RECALCULATE ME
+	constant MINUS_INV_CORDIC_GAIN : real := -0.60729980468750;  -- RECALC
+        constant PI : real := 3.14159265358979;
+        constant MINUS_PI : real := -3.14159265358979;
+        constant HALF_PI : real := 1.5708;
+        constant MINUS_HALF_PI : real := -1.5708;
 
 	-- Filter constants
         constant PHASE_LOOP_PI_I_CONST : real := 1000.0;
         constant PHASE_LOOP_PI_P_CONST : real := 100.0;
 
+        function to_integer ( val : real; width : natural; prec : natural ) return integer;
+        function to_vector ( val : real; width : natural; prec : natural ) return std_logic_vector;
+        
         function to_pipeline_integer ( val : real ) return integer;
-        function to_pipeline_vector ( val : real ) return std_logic_vector(PIPELINE_WIDTH - 1 downto 0);
+        function to_pipeline_vector ( val : real ) return pipeline_vector;
+        function to_ext_pipeline_integer ( val : real ) return integer;
+        function to_ext_pipeline_vector ( val : real ) return ext_pipeline_vector;
 end common;
 
 
 package body common is
-        function to_pipeline_integer ( val : real ) return integer is
+        function to_integer ( val : real; width : natural; prec : natural ) return integer is
                 variable tmp_val : integer;
+                variable max_integer, min_integer : integer;
         begin
-                tmp_val := integer(round(val * real(PIPELINE_PREC_WEIGHT)));
-                if (tmp_val > pipeline_integer'high) then
-                        return pipeline_integer'high;
-                elsif (tmp_val < pipeline_integer'low) then
-                        return pipeline_integer'low;
+                tmp_val := integer(round(val * (2.0 ** real(prec))));
+                max_integer := integer(round(2.0 ** real(width - 1)));
+                min_integer := -max_integer - 1;
+                if (tmp_val > max_integer) then
+                        assert false report "saturating pipeline(w: " & integer'image(width) & ", p: " & integer'image(prec) & ") with value " & real'image(val) severity warning;
+                        return max_integer;
+                elsif (tmp_val < min_integer) then
+                        assert false report "neg. saturating pipeline(w: " & integer'image(width) & ", p: " & integer'image(prec) & ") with value " & real'image(val) severity warning;
+                        return min_integer;
                 else
                         return tmp_val;
                 end if;
         end;
 
-        function to_pipeline_vector ( val : real ) return std_logic_vector(PIPELINE_WIDTH - 1 downto 0) is
+        function to_vector ( val : real; width : natural; prec : natural ) return std_logic_vector is
         begin
-                return std_logic_vector(to_signed(to_pipeline_integer(val, PIPELINE_WIDTH)));
+                return std_logic_vector(to_signed(to_integer(val, width, prec), width));
+        end;
+                
+        function to_pipeline_integer ( val : real ) return integer is
+        begin
+                return to_integer(val, PIPELINE_WIDTH, PIPELINE_PREC);
+        end;
+
+        function to_pipeline_vector ( val : real ) return pipeline_vector is
+        begin
+                return to_vector(val, PIPELINE_WIDTH, PIPELINE_PREC);
+        end;
+
+        function to_ext_pipeline_integer ( val : real ) return integer is
+        begin
+                return to_integer(val, EXT_PIPELINE_WIDTH, EXT_PIPELINE_PREC);
+        end;
+
+        function to_ext_pipeline_vector ( val : real ) return ext_pipeline_vector is
+        begin
+                return to_vector(val, EXT_PIPELINE_WIDTH, EXT_PIPELINE_PREC);
         end;
 end common;
