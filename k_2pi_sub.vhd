382b496085ff6457b0d854f39fd3485c3ebfe044
Jacobo Cabaleiro <obmun.h@gmail.com>
2007-10-15 10:47:38 +0200

    Fully parametrize k_2pi_sub. Adapt it to use new real <-> pipeline conversion functions

--- k_2pi_sub.vhd
+++ k_2pi_sub.vhd
@@ -8,60 +8,83 @@
 -- Project Name:   
 -- Target Device:  
 -- Tool versions:  7.1i
--- Description:
--- | Módulo que RESTA a la entrada 2*PI.
--- | El uso de esta entidad es en aquellos casos en los que se quiere mantener
--- | 0 <= |phi| < PI, restando 2*PI cuado phi acabe de superar el límite de PI.
--- | Dada la codificación elegida (coma fija FX3.16), 2*PI NO es un valor
--- | representable. Por eso internamente se trabaja con un bit más (FX4.16) para
--- | luego truncar el resultado a FX3.16 => EL RESULTADO SÓLO ES VÁLIDO EN LOS
--- | casos supuestos.
+--
+-- === Brief description ===
+-- Módulo que RESTA a la entrada 2*PI.
+--
+-- === Description ===
+-- El uso de esta entidad es en aquellos casos en los que se quiere mantener
+-- 0 <= |phi| < PI, restando 2*PI cuado phi acabe de superar el límite de PI.
+--
+-- La codificación elegida originalmente (FX3.16) obligaba a la utilización de
+-- este elemento, ya que 2*PI no era un valor representable en el PIPELINE. La
+-- implementación del mismo se realizaba aumentando la anchura del pipeline original.
+--
+-- Ahora mismo, el ancho de pipeline es 100% flexible. Las primeras pruebas nos
+-- parecen mostrar que al menos deberemos usar un FX4.16 o incluso FX5.16 (con
+-- tan sólo 9 bits para la parte fraccionaria), todas con capacidad para representar 2*PI.
+--
+-- Preferimos sin embargo mantener la táctica de que TODOS los ángulos vayan en
+-- argumento principal (-180 < 0 <= 180), y seguimos utilizando este elemento.
 -- 
 -- Dependencies:
 -- 
--- Revision:
+-- === TODO ===
+-- == Check if constant adder is using "simplified logic".
+-- 1.- A 17 bit adder is CORRECTLY inferred. But I'M ADDING A CONSTANT.
+--      Check in the final implementation is design is simplified with k in mind.
+--
+-- === Revision ===
+-- Revision 0.02 - Reworked: adapted to new to_vector and to_integer system,
+-- and made it automatically detect if pipeline width must be extended.
 -- Revision 0.01 - File Created
--- 
--- Additional Comments:
--- 
--- TODO:
--- | 1.- A 17 bit adder is CORRECTLY inferred. But I'M ADDING A CONSTANT.
--- |     Check in the final implementation is design is simplified with k in mind.
 --------------------------------------------------------------------------------
 library IEEE;
 use WORK.COMMON.all;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity k_2pi_sub is
 	generic (
-		width : natural := PIPELINE_WIDTH
-	);
+		width : natural := PIPELINE_WIDTH;
+                prec : natural := PIPELINE_PREC);
 	port (
 		i : in std_logic_vector(width - 1 downto 0);
 		en : in std_logic;
-		o : out std_logic_vector(width - 1 downto 0)
-	);
+		o : out std_logic_vector(width - 1 downto 0));
 end k_2pi_sub;
 
-architecture alg of k_2pi_sub is
+architecture beh of k_2pi_sub is
+        constant magn_size : natural := PIPELINE_WIDTH - PIPELINE_PREC - 1;
 begin
-	sub : process(i, en)
-		variable big_in, big_res : std_logic_vector(width downto 0);
-	begin
-		big_in(width - 1 downto 0) := i;
-		big_in(width) := i(width - 1); -- Sign extension
-		big_res := std_logic_vector(signed(big_in) + signed(MINUS_TWO_PI_FX417_V));
-		if (en = '1') then
-			o(width - 2 downto 0) <= big_res(width - 2 downto 0);
-			o(width - 1) <= big_res(width); -- SIGN EXTENSION
-		else
-			o <= i;
-		end if;
-	end process;
-end alg;
+        extended_generate : if magn_size < min_magn_size(TWO_PI) generate
+        begin
+                sub : process(i, en)
+                        variable big_in, big_res : std_logic_vector(min_magn_size(TWO_PI) + 1 + PIPELINE_PREC downto 0);
+                begin
+                        big_in(width - 1 downto 0) := i;
+                        big_in(big_in'high downto width) := (others => i(width - 1)); -- Sign extension
+                        big_res := std_logic_vector(signed(big_in) + signed(to_vector(MINUS_TWO_PI, big_res'length, PIPELINE_PREC)));
+                        if (en = '1') then
+                                o(width - 2 downto 0) <= big_res(width - 2 downto 0);
+                                o(width - 1) <= big_res(width); -- SIGN
+                                                                -- EXTENSION
+                                                                -- (compression :)
+                        else
+                                o <= i;
+                        end if;
+                end process;
+        end generate;
+
+        normal_generate : if magn_size >= min_magn_size(TWO_PI) generate
+        begin
+                sub : process(i, en)
+                begin
+                        if (en = '1') then
+                                o <= std_logic_vector(signed(i) + signed(to_vector(MINUS_TWO_PI, width, prec)));
+                        else
+                                o <= i;
+                        end if;
+                end process;
+        end generate;
+end beh;
