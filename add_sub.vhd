a6ea93e8d92912dc342d182f933bc41c132a9d8a
Jacobo Cabaleiro <obmun.h@gmail.com>
2007-10-25 13:56:59 +0200

    Removed unnecesary comments from file.

--- add_sub.vhd
+++ add_sub.vhd
@@ -22,11 +22,6 @@ use WORK.COMMON.ALL;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity add_sub is
      generic (
           width : natural := PIPELINE_WIDTH
