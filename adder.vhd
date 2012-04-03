73c3214f1dfba097f264f42a3cd02f145eed45f4
Jacobo Cabaleiro <obmun.h@gmail.com>
2012-04-03 14:14:12 +0200

    Reindented adder.vhd using last identation rules

--- adder.vhd
+++ adder.vhd
@@ -11,19 +11,19 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 entity adder is
-        -- rev 0.01
-	generic (
-		width : natural := PIPELINE_WIDTH);
-	port (
-		a, b: in std_logic_vector(width - 1 downto 0);
-		o: out std_logic_vector(width - 1 downto 0);
-		f_ov, f_z: out std_logic);
+     -- rev 0.01
+     generic (
+          width : natural := PIPELINE_WIDTH);
+     port (
+          a, b: in std_logic_vector(width - 1 downto 0);
+          o: out std_logic_vector(width - 1 downto 0);
+          f_ov, f_z: out std_logic);
 end adder;
 
 architecture alg of adder is
 begin
-	process(a, b)
-	begin
-		o <= std_logic_vector(signed(a) + signed(b));
-	end process;
+     process(a, b)
+     begin
+          o <= std_logic_vector(signed(a) + signed(b));
+     end process;
 end architecture alg;
