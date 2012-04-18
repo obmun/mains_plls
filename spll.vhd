d51be94bc44bc1682c2a90b55bae95d6a7e3b431
Jacobo Cabaleiro <obmun.h@gmail.com>
2012-04-18 16:42:29 +0200

    Commiting the _almost_ final top level "spll" entity implementing the ALL-PLL design. Attached are the corresponding cosimulation Simulink model and the corrected ModelSim project.
    DESIGN IS NOT FULLY WORKING. I've found, under normal test signals, NEGATIVE VALUES for the (1/A)^2 value inside the ampl. loop, so sqrt entity gets into an error state while trying to calculate those values.

--- spll.vhd
+++ spll.vhd
@@ -1,12 +1,12 @@
 --------------------------------------------------------------------------------
--- Revision:
--- Revision 0.01 - File Created
--- 
+-- *** Brief description ***
+--
+-- ALL-PLL (also known as 'spll') full design, using the amplitude loop and phase loop
+--
 --------------------------------------------------------------------------------
 library IEEE;
 library WORK;
 use IEEE.STD_LOGIC_1164.all;
-use IEEE.NUMERIC_STD.all;
 use WORK.COMMON.all;
 
 
@@ -20,37 +20,58 @@ end spll;
 
 
 architecture alg of spll is
-     type state_t is (ST_SAMPLE_WAIT, ST_RUN);
+     type state_t is (ST_RST, -- State required for waiting for individual elements reset [ampl_loop
+                              -- can take some time]; 1st part of reset states (see ST_WAIT_FOR_RST
+                              -- also).
+                      ST_WAIT_FOR_RST ,
+                      ST_SAMPLE_WAIT,
+                      ST_RUN);
 
-     signal st : state_t := ST_SAMPLE_WAIT;
+     signal st : state_t;
 
-     signal run_s, ampl_done_s, phase_done_s: std_logic;
+     signal run_s, rst_s, ampl_done_s, ampl_done_pulsed_s, phase_done_s, done_pulser_en_s: std_logic;
      signal our_signal_s, norm_in_signal_s : std_logic_vector(PIPELINE_WIDTH - 1 downto 0);
 begin
 
      ampl_loop_i : entity work.ampl_loop(alg)
           port map (
-               clk => clk, run => ampl_done_s, rst => rst,
+               clk => clk, run => run_s, rst => rst_s,
                in_signal => in_signal, our_signal => our_signal_s,
                norm_in_signal => norm_in_signal_s,
+               done => ampl_done_s);
+
+     done_pulser_i : entity work.done_pulser(beh)
+          port map (
+               clk => clk,
+               en  => done_pulser_en_s,
+               rst => rst,
+               i   => ampl_done_s,
+               o   => ampl_done_pulsed_s);
+     
+     phase_loop_i : entity work.phase_loop(alg)
+          port map (
+               clk => clk, run => ampl_done_pulsed_s, rst => rst_s,
+               norm_input => norm_in_signal_s,
+               phase => phase, norm_sin => our_signal_s,
                done => phase_done_s
                );
 
-     phase_loop_i : entity work.phase_loop(alg)
-          port map (
-               clk => clk, run => run_s, rst => rst,
-               norm_input => norm_in_signal_s,
-               phase => phase, norm_sin => our_signal_s,
-               done => ampl_done_s
-               );
-
-     state_ctrl : process(clk)
+     
+     state_ctrl : process(clk, sample, ampl_done_s, phase_done_s)
      begin
           if (rising_edge(clk)) then
                if (rst = '1') then
-                    st <= ST_SAMPLE_WAIT;
+                    st <= ST_RST;
                else
                     case st is
+                         when ST_RST =>
+                              st <= ST_WAIT_FOR_RST;
+                         when ST_WAIT_FOR_RST =>
+                              if (ampl_done_s = '1' and phase_done_s = '1') then
+                                   st <= ST_SAMPLE_WAIT;
+                              else
+                                   st <= ST_WAIT_FOR_RST;
+                              end if;
                          when ST_SAMPLE_WAIT =>
                               if (sample = '0') then
                                    st <= ST_SAMPLE_WAIT;
@@ -58,7 +79,7 @@ begin
                                    st <= ST_RUN;
                               end if;
                          when ST_RUN =>
-                              if (phase_done_s = '0') then
+                              if (phase_done_s = '0' or ampl_done_s = '0') then
                                    st <= ST_RUN;
                               else
                                    if (sample = '0') then
@@ -72,18 +93,39 @@ begin
           end if;
      end process;
 
-     signal_gen : process(st, sample, phase_done_s)
-     -- I need to control:
-     -- * run_s
-     -- * done
+     
+     signal_gen : process(st, sample, phase_done_s, rst)
      begin
           case st is
+               when ST_RST =>
+                    done <= '0';
+                    run_s <= '0';
+                    rst_s <= '1';
+                    done_pulser_en_s <= '0';
+               when ST_WAIT_FOR_RST =>
+                    done <= '0';
+                    run_s <= '0';
+                    rst_s <= '0';
+                    done_pulser_en_s <= '0'; -- Avoid that the done rising edge to launch a run of
+                                             -- the chained phase_loop during reset, as the done is
+                                             -- not signaling the finish of a calculation but the
+                                             -- finish of the reset operation.
                when ST_SAMPLE_WAIT =>
                     done <= '1';
                     run_s <= sample;
+                    rst_s <= rst;
+                    done_pulser_en_s <= '1';
                when ST_RUN =>
-                    done <= phase_done_s;
-                    run_s <= phase_done_s and sample;
+                    done <= ampl_done_s and phase_done_s;
+                    run_s <= '0';
+                    rst_s <= rst;
+                    done_pulser_en_s <= '1';
+               when others =>
+                    done <= '0'; -- Should be '-'
+                    run_s <= '0';
+                    rst_s <= '0';
+                    done_pulser_en_s <= '0';
+                    report "INCORRECT STATE ON spll entity FSM" severity failure;
           end case;
      end process;
 
